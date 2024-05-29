c755399db839dbcfe7f5d75bddbb041870cf2910
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-05-29 19:38:00 -0300

    feat: add PLAY GAME button to open GameTab

--- app/_components/findGame.tsx
+++ app/_components/findGame.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 const FindGame = () => {
   return (
-    <Button className="flex justify-around items-center absolute bottom-5 right-16 z-50 bg-emerald-600 w-72 p-2 border-t border-l border-white shadow-md shadow-sky-900 hover:bg-emerald-500">
-      <h1 className="text-2xl font-bold tracking-widest text-white">
+    <Button className="flex justify-around items-center absolute bottom-5 right-16 z-50 bg-emerald-600 w-72 border-t border-l border-white shadow-md shadow-sky-900 hover:bg-emerald-500">
+      <h1 className="text-2xl font-bold tracking-widest text-white ">
         JOGAR DOTA
       </h1>
     </Button>
