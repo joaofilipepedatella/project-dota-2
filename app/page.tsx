8fa3f720a9840b06be02469999d9ebf16e827e22
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-05-31 15:33:00 -0300

    feat: add backgroundImage to body

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
 import Image from "next/image";
 import ActualGroup from "./_components/groupActual";
-import FindGame from "./_components/findGame";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import GameTab from "./_components/gameTab";
+import GameTab from "./_components/_gameTab/gameTab";
 
 export default function Home() {
   return (
-    <div className="min-w-full min-h-full">
-      <div>
-        <Image
-          src="/dota-2-bg.jpg"
-          layout="fill"
-          objectFit="cover"
-          alt="dota 2 background"
-        />
-      </div>
-
+    <div className="w-full h-full">
       <ActualGroup />
       <GameTab />
     </div>
