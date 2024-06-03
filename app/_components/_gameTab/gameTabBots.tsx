4c65520deda6dc281eaf20a0f62799612de16673
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-06-03 20:28:01 -0300

    feat: finished tab for Bots Game

--- app/_components/_gameTab/gameTabBots.tsx
+++ app/_components/_gameTab/gameTabBots.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
@@ -13,8 +16,11 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import Link from "next/link";
+import { useState } from "react";
 
 const JogarContraBots = () => {
+  const [gameMode, setGameMode] = useState("cooperative");
+
   return (
     <>
       <AccordionItem value="item-3" className="bg-slate-800/80 border-none">
@@ -28,114 +34,212 @@ const JogarContraBots = () => {
                   value="cooperative"
                   id="cooperative"
                   className="bg-black"
+                  onClick={() => setGameMode("cooperative")}
                 />
                 <Label htmlFor="cooperative">COOPERATIVO</Label>
               </div>
               <div className="flex gap-1">
-                <RadioGroupItem value="solo" id="solo" className="bg-black" />
+                <RadioGroupItem
+                  value="solo"
+                  id="solo"
+                  className="bg-black"
+                  onClick={() => setGameMode("solo")}
+                />
                 <Label htmlFor="solo">SOLO</Label>
               </div>
             </RadioGroup>
           </div>
-          {/* SCRIPTS */}
-          <div className="w-full border-b p-2">
-            <div className="flex justify-between items-center w-full">
-              <h5>SCRIPTS DE BOTS</h5>
-              <Link
-                href="https://steamcommunity.com/app/570/workshop/"
-                className="border-b border-stone-300 text-xs"
-              >
-                Ver na oficina
-              </Link>
-            </div>
-            <div className="w-full p-2">
-              <Select>
-                <SelectTrigger className="w-full bg-black">
-                  <SelectValue placeholder="Selecione o Script" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="padrao">Bots Padrão</SelectItem>
-                  <SelectItem value="oficina">Bots Oficina</SelectItem>
-                  <SelectItem value="d2school">Bots Dota2School</SelectItem>
-                  <SelectItem value="liquid">Bots Liquid</SelectItem>
-                  <SelectItem value="secret">Bots Secret</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-          {/* DIFICULDADE / EQUIPE */}
-          <div className="w-full flex ">
-            <div className="flex flex-col w-1/2 p-2">
-              <h5>DIFICULDADE</h5>
-              <form className="gap-1 p-1">
-                <RadioGroup defaultValue="easy" className="gap-1 p-1">
+
+          {gameMode == "solo" ? (
+            <>
+              <div className="w-full border-b p-2">
+                <div className="flex justify-between items-center w-full">
+                  <h5>SCRIPTS DE BOTS</h5>
+                  <Link
+                    href="https://steamcommunity.com/app/570/workshop/"
+                    className="border-b border-stone-300 text-xs"
+                  >
+                    Ver na oficina
+                  </Link>
+                </div>
+                <div className="w-full p-2">
+                  <Select>
+                    <SelectTrigger className="w-full bg-black">
+                      <SelectValue placeholder="Selecione o Script" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="padrao">Bots Padrão</SelectItem>
+                      <SelectItem value="oficina">Bots Oficina</SelectItem>
+                      <SelectItem value="d2school">Bots Dota2School</SelectItem>
+                      <SelectItem value="liquid">Bots Liquid</SelectItem>
+                      <SelectItem value="secret">Bots Secret</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+              <div className="w-full flex ">
+                <div className="flex flex-col w-1/2 p-2">
+                  <h5>DIFICULDADE</h5>
+                  <form className="gap-1 p-1">
+                    <RadioGroup defaultValue="easy" className="gap-1 p-1">
+                      <div className="flex gap-1">
+                        <RadioGroupItem
+                          value="easy"
+                          id="easy"
+                          className="bg-black"
+                        />
+                        <Label htmlFor="easy">FÁCIL</Label>
+                      </div>
+                      <div className="flex gap-1">
+                        <RadioGroupItem
+                          value="medium"
+                          id="medium"
+                          className="bg-black"
+                        />
+                        <Label htmlFor="medium">MÉDIO</Label>
+                      </div>
+                      <div className="flex gap-1">
+                        <RadioGroupItem
+                          value="hard"
+                          id="hard"
+                          className="bg-black"
+                        />
+                        <Label htmlFor="hard">DIFÍCIL</Label>
+                      </div>
+                      <div className="flex gap-1">
+                        <RadioGroupItem
+                          value="unfair"
+                          id="unfair"
+                          className="bg-black"
+                        />
+                        <Label htmlFor="unfair">INJUSTO</Label>
+                      </div>
+                    </RadioGroup>
+                  </form>
+                </div>
+                <div className="flex flex-col w-1/2 p-2">
+                  <h5>EQUIPE</h5>
+                  <form className="gap-1 p-1">
+                    <RadioGroup defaultValue="radiant" className="gap-1 p-1">
+                      <div className="flex gap-1">
+                        <RadioGroupItem
+                          value="radiant"
+                          id="radiant"
+                          className="bg-black"
+                        />
+                        <Label htmlFor="radiant">ILUMINADOS</Label>
+                      </div>
+                      <div className="flex gap-1">
+                        <RadioGroupItem
+                          value="dire"
+                          id="dire"
+                          className="bg-black"
+                        />
+                        <Label htmlFor="dire">TEMIDOS</Label>
+                      </div>
+                      <div className="flex gap-1">
+                        <RadioGroupItem
+                          value="random"
+                          id="random"
+                          className="bg-black"
+                        />
+                        <Label htmlFor="random">ALEATÓRIA</Label>
+                      </div>
+                    </RadioGroup>
+                  </form>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="w-full p-2">
+                <div className="flex items-center w-full mb-2">
+                  <h5>SCRIPTS DE BOTS</h5>
+                </div>
+                <div className="flex flex-col gap-2 w-full justify-center">
                   <div className="flex gap-1">
-                    <RadioGroupItem
-                      value="easy"
+                    <Checkbox
+                      id="original"
+                      className="bg-black border-2 border-slate-400"
+                    />
+                    <Label htmlFor="original">
+                      <span>BOTS PADRÃO</span>
+                    </Label>
+                  </div>
+                  <div className="flex gap-1">
+                    <Checkbox
+                      id="rankedAI"
+                      className="bg-black border-2 border-slate-400"
+                    />
+                    <Label htmlFor="rankedAI">
+                      <span>RANKED MATCHMAKING AI</span>
+                    </Label>
+                  </div>
+                  <div className="flex gap-1">
+                    <Checkbox
+                      id="expFurious"
+                      className="bg-black border-2 border-slate-400"
+                    />
+                    <Label htmlFor="expFurious">
+                      <span>BOT EXPERIMENT CREDIT:FURIOUSPUPPY</span>
+                    </Label>
+                  </div>
+                  <div className="flex border-b pb-2 gap-1">
+                    <Checkbox
+                      id="chinaAI"
+                      className="bg-black border-2 border-slate-400"
+                    />
+                    <Label htmlFor="chinaAI">
+                      <span>CHINA AI</span>
+                    </Label>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full p-2">
+                <div className="flex items-center w-full mb-2">
+                  <h5>DIFICULDADE</h5>
+                </div>
+                <div className="flex flex-col gap-2 w-full justify-center">
+                  <div className="flex gap-1">
+                    <Checkbox
                       id="easy"
-                      className="bg-black"
+                      className="bg-black border-2 border-slate-400"
                     />
-                    <Label htmlFor="easy">FÁCIL</Label>
+                    <Label htmlFor="easy">
+                      <span>FÁCIL</span>
+                    </Label>
                   </div>
                   <div className="flex gap-1">
-                    <RadioGroupItem
-                      value="medium"
+                    <Checkbox
                       id="medium"
-                      className="bg-black"
+                      className="bg-black border-2 border-slate-400"
                     />
-                    <Label htmlFor="medium">MÉDIO</Label>
+                    <Label htmlFor="medium">
+                      <span>MÉDIO</span>
+                    </Label>
                   </div>
                   <div className="flex gap-1">
-                    <RadioGroupItem
-                      value="hard"
+                    <Checkbox
                       id="hard"
-                      className="bg-black"
+                      className="bg-black border-2 border-slate-400"
                     />
-                    <Label htmlFor="hard">DIFÍCIL</Label>
+                    <Label htmlFor="hard">
+                      <span>DIFÍCIL</span>
+                    </Label>
                   </div>
                   <div className="flex gap-1">
-                    <RadioGroupItem
-                      value="unfair"
+                    <Checkbox
                       id="unfair"
-                      className="bg-black"
+                      className="bg-black border-2 border-slate-400"
                     />
-                    <Label htmlFor="unfair">INJUSTO</Label>
+                    <Label htmlFor="unfair">
+                      <span>INJUSTO</span>
+                    </Label>
                   </div>
-                </RadioGroup>
-              </form>
-            </div>
-            <div className="flex flex-col w-1/2 p-2">
-              <h5>EQUIPE</h5>
-              <form className="gap-1 p-1">
-                <RadioGroup defaultValue="radiant" className="gap-1 p-1">
-                  <div className="flex gap-1">
-                    <RadioGroupItem
-                      value="radiant"
-                      id="radiant"
-                      className="bg-black"
-                    />
-                    <Label htmlFor="radiant">ILUMINADOS</Label>
-                  </div>
-                  <div className="flex gap-1">
-                    <RadioGroupItem
-                      value="dire"
-                      id="dire"
-                      className="bg-black"
-                    />
-                    <Label htmlFor="dire">TEMIDOS</Label>
-                  </div>
-                  <div className="flex gap-1">
-                    <RadioGroupItem
-                      value="random"
-                      id="random"
-                      className="bg-black"
-                    />
-                    <Label htmlFor="random">ALEATÓRIA</Label>
-                  </div>
-                </RadioGroup>
-              </form>
-            </div>
-          </div>
+                </div>
+              </div>
+            </>
+          )}
         </AccordionContent>
       </AccordionItem>
     </>
