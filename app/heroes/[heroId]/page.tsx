5f1bf0e40b1659bae4d2ef53a638c10b4dbcd96e
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-06-11 16:04:11 -0300

    feat: created page to see a hero individualy

---
+++ app/heroes/[heroId]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import heroes from "@/public/heroes.json";
+import HeroInfo from "./_components/heroInfo";
+import HeaderHeroes from "../_components/header";
+
+const findHeroById = (id: number) => {
+  return heroes.find((hero) => hero.id === id);
+};
+
+const HeroPage = () => {
+  const { heroId } = useParams();
+  const heroIdNumber = Number(heroId);
+  const hero = findHeroById(heroIdNumber);
+  if (!hero) {
+    return <div>Herói não encontrado</div>;
+  }
+
+  return (
+    <>
+      <HeaderHeroes />
+      <HeroInfo hero={hero} />
+    </>
+  );
+};
+
+export default HeroPage;
