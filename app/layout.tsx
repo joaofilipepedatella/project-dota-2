614eedb02660755cdd4375939b143b1ac56b3787
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-06-11 16:00:40 -0300

    feat: add some styles to global CSS and add provider to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/_header/header";
+import { HeroProvider } from "./contexts/HeroContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <HeroProvider>
+          <Header />
+          {children}
+        </HeroProvider>
       </body>
     </html>
   );
