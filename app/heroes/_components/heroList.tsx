1b741719705f513a33eb4d04e1c0e93912bfbf2f
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-06-11 16:03:12 -0300

    feat: created component to render heroes by their attributes

--- app/heroes/_components/heroList.tsx
+++ app/heroes/_components/heroList.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useHero } from "@/app/contexts/HeroContext";
 
-interface Hero {
+export interface Hero {
   id: number;
   localized_name: string;
   primary_attr: string;
+}
+
+interface HeroStats {
+  id: number;
+  localized_name: string;
+  img: string; // URL da imagem do herói
 }
 
 interface HeroesListProps {
@@ -19,13 +28,15 @@ const attributeNames: { [key: string]: string } = {
 };
 
 const HeroList: React.FC<HeroesListProps> = ({ heroes, attribute }) => {
+  const { selectedHeroId, setHeroId } = useHero();
+
   const bgColor =
     attribute === "str"
-      ? "bg-red-200"
+      ? "bg-red-300"
       : attribute === "agi"
-      ? "bg-green-200"
-      : attribute === " int"
-      ? "bg-blue-200"
+      ? "bg-green-300"
+      : attribute === "int"
+      ? "bg-blue-300"
       : "bg-purple-200";
   const textColor =
     attribute === "str"
@@ -37,18 +48,51 @@ const HeroList: React.FC<HeroesListProps> = ({ heroes, attribute }) => {
       : "text-purple-700";
 
   const attributeName = attributeNames[attribute];
+  const [heroStats, setHeroStats] = useState<HeroStats[]>([]);
+
+  useEffect(() => {
+    const fetchHeroStats = async () => {
+      try {
+        const response = await fetch("https://api.opendota.com/api/heroStats/");
+        if (!response.ok) {
+          throw new Error("Erro ao buscar os stats dos heróis");
+        }
+        const data = await response.json();
+        setHeroStats(data);
+      } catch (error) {
+        console.error("Erro ao buscar os stats dos heróis:", error);
+      }
+    };
+
+    fetchHeroStats();
+  }, []);
+
+  const sortedHeroes = heroes
+    .filter((hero) => hero.primary_attr === attribute)
+    .sort((a, b) => a.localized_name.localeCompare(b.localized_name));
 
   return (
     <div className={`p-5 ${bgColor} rounded-lg mt-4`}>
-      <h2 className={`text-lg font-bold ${textColor}`}>{attributeName}</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {heroes
-          .filter((hero) => hero.primary_attr === attribute)
-          .map((hero) => (
-            <div key={hero.id} className={`p-2 ${bgColor} rounded shadow`}>
-              {hero.localized_name}
-            </div>
-          ))}
+      <h2 className={`text-lg font-bold ${textColor} p-1`}>{attributeName}</h2>
+      <div className="grid grid-cols-8 gap-1">
+        {sortedHeroes.map((hero) => {
+          const heroStat = heroStats.find((stat) => stat.id === hero.id);
+          if (!heroStat) return null;
+          return (
+            <Link key={hero.id} href={`/heroes/${hero.id}`}>
+              <Button
+                className={`${bgColor} rounded shadow p-0 h-16 w-24 flex justify-center items-center text-center hover:bg-transparent`}
+                onClick={() => setHeroId(hero.id)}
+              >
+                <img
+                  src={`https://cdn.dota2.com${heroStat.img}`}
+                  alt={hero.localized_name}
+                  className="w-24 h-16 rounded-sm"
+                />
+              </Button>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
