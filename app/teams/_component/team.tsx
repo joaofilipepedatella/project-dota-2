8e62fd49f60b0ea1dd24c6a936652783ea7a9029
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-06-19 19:13:43 -0300

    feat: add Link component to direct user to Team Page

--- app/teams/_component/team.tsx
+++ app/teams/_component/team.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ProPlayer, Team } from "../page";
 import TeamPlayerComponent from "./teamPlayers";
+import Link from "next/link";
 
 interface TeamProps {
   team: Team;
@@ -36,19 +37,21 @@ const TeamComponent: React.FC<TeamProps> = ({ team }) => {
 
   return (
     <div className="flex flex-col bg-gradient-to-r from-slate-600/10 to-transparent my-2 p-4 gap-2">
-      <div className="flex items-center gap-4">
-        <img
-          src={team.logo_url}
-          alt={team.name}
-          width={100}
-          height={50}
-          className="bg-black"
-        />
-        <div>
-          <h2>{team.name}</h2>
-          <p>{team.tag}</p>
+      <Link key={team.team_id} href={`/teams/${team.team_id}`}>
+        <div className="flex items-center gap-4">
+          <img
+            src={team.logo_url}
+            alt={team.name}
+            width={100}
+            height={50}
+            className="bg-black"
+          />
+          <div>
+            <h2>{team.name}</h2>
+            <p>{team.tag}</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="flex flex-col gap-4 p-4">
         <h3>Players:</h3>
         {loading ? (
