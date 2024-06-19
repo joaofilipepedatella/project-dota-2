295b481126beedd7774c80d1cc381ae05f32b1db
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-06-19 19:15:49 -0300

    remove: remove console.log

--- app/services/heroService.tsx
+++ app/services/heroService.tsx
@@ -2,7 +2,7 @@ const fetchHeroData = async () => {
   try {
     const response = await fetch("/heroes.json");
     const heroStats = await response.json();
-    console.log("HeroStats:", heroStats); // Adicionei um console.log aqui
+
     return heroStats;
   } catch (error) {
     console.error(error);
@@ -10,7 +10,6 @@ const fetchHeroData = async () => {
 };
 
 const findHeroById = (heroId: number, heroAttributes: any[]) => {
-  console.log("ID:", heroId); // Adicionei um console.log aqui
   const hero = heroAttributes.find((hero) => hero.id === heroId);
   return hero;
 };
